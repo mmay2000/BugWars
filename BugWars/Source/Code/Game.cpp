7baa13c37f00c8fdee285d426273963622723447
mmay2000 <102404342+mmay2000@users.noreply.github.com>
2022-11-07 19:12:06 +0300

    make it work | make it right | firts attempt in smart mapping

--- BugWars/Source/Code/Game.cpp
+++ BugWars/Source/Code/Game.cpp
@@ -13,19 +13,69 @@ Game::Game()
 				 [] {return new Bullet; } })
 {
 	g_Game = this;
+	for(int i = 0; i < 100; ++i)
+	{
+		for (int j = 0; j < 100; ++j)
+		{
+			if ((i + 1) < 100)
+			{
+				gameMap[i][j].upNeighbor = &gameMap[i + 1][j];
+
+				if ((j + 1) < 100)
+					gameMap[i][j].RUDNeighbor = &gameMap[i + 1][j + 1];
+				else
+					gameMap[i][j].RUDNeighbor = nullptr;
+
+				if ((j - 1) > -1)
+					gameMap[i][j].LUDNeighbor = &gameMap[i + 1][j - 1];
+				else
+					gameMap[i][j].LUDNeighbor = nullptr;
+			}
+			else
+				gameMap[i][j].upNeighbor = nullptr;
+
+			if ((j + 1) < 100)
+				gameMap[i][j].rightNeighbor = &gameMap[i][j + 1];
+			else
+				gameMap[i][j].rightNeighbor = nullptr;
+
+			if ((i - 1) > -1)
+			{
+				gameMap[i][j].downNeighbor = &gameMap[i - 1][j];
+
+				if ((j + 1) < 100)
+					gameMap[i][j].RDDNeighbor = &gameMap[i - 1][j + 1];
+				else
+					gameMap[i][j].RDDNeighbor = nullptr;
+
+				if ((j - 1) > -1)
+					gameMap[i][j].LDDNeighbor = &gameMap[i - 1][j - 1];
+				else
+					gameMap[i][j].LDDNeighbor = nullptr;
+			}
+			else
+				gameMap[i][j].downNeighbor = nullptr;
+
+			if ((j - 1) > -1)
+				gameMap[i][j].leftNeighbor = &gameMap[i][j - 1];
+			else
+				gameMap[i][j].leftNeighbor = nullptr;
+		}
+	}
 }
 
 void Game::OnUpdate(float dt)
 {
 	PIXScopedEvent(PIX_COLOR_INDEX(5), __FUNCTION__);
-	for (auto obj : objects)
+	for (GameObject* obj : objects)
 		if (!obj->disabled)
 			obj->Update(dt);
+
 }
 
 void Game::OnRender() const
 {
-	for (auto obj : objects)
+	for (GameObject* obj : objects)
 		if (obj->visible)
 			DoRender(obj);
 }
@@ -33,10 +83,20 @@ void Game::OnRender() const
 void Game::AddObject(GameObject* object)
 {
 	objects.push_back(object);
-	if (object->GetRTTI() == Bug::s_RTTI)
-		Log("I'm a bug");
+	int indX = static_cast<int>(trunc(object->position.x));
+	int indY = static_cast<int>(trunc(object->position.y));
+	gameMap[indX][indY].objects.push_back(object);
+//	if (object->GetRTTI() == Bug::s_RTTI)
+	//	Log("I'm a bug");
 }
 
 void Game::OnBugsSpawned()
 {
+
+}
+
+Game::~Game()
+{
+	for (GameObject* obj : objects)
+		delete(obj);
 }
--- BugWars/Source/Code/Tank.cpp
+++ BugWars/Source/Code/Tank.cpp
@@ -7,20 +7,50 @@
 
 IMPLEMENT_RTTI(Tank);
 
+
+Tank::Tank()
+{
+	visible = true;
+	disabled = false;
+}
+
 void Tank::OnUpdate(float dt)
 {
 }
 
 BugBase* Tank::GetBugToShoot() const
 {
-	for (auto obj : g_Game->objects)
-		if (auto bug = dynamic_cast<Bug*>(obj))
-			return bug;
+	Bug* target = nullptr;
+	float min_dist = std::numeric_limits<float>::max();
+	for (GameObject* object : g_Game->objects)
+	{
+		if (object->GetRTTI() == Bug::s_RTTI)
+		{
+			if (object->disabled)
+				continue;
 
-	return nullptr;
+			float dist = position.Distance(object->position);
+			if (dist < min_dist)
+			{
+				min_dist = dist;
+				target = static_cast<Bug*>(object);
+			}
+		}
+	}
+
+	if (min_dist > 200.f)
+		target = nullptr;
+	return target;
 }
 
 Point Tank::CalcShootDirection(Point target_pos, Point target_dir, float target_vel, float bullet_vel) const
 {
-	return { 1, 0 };
+	Point meetingDir = target_pos - position;
+
+	if (meetingDir.x * target_dir.y == meetingDir.y * target_dir.x)
+		return meetingDir;
+	
+	meetingDir = (target_pos - position) * 1.5f -  target_dir * target_vel / bullet_vel / 2.0f;
+	meetingDir = meetingDir + target_dir * target_vel * (position.Distance(target_pos) / bullet_vel);
+	return meetingDir;
 }
